cb04b75377a0394d9224d8f6a85c14bea33ebdc4
joonas175 <joonas.salojarvi@tuni.fi>
2019-12-18 21:21:58 +0200

    MainScreen WIP

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
-
-
+import { FlatList } from 'react-native-gesture-handler';
+import { NavigationEvents } from 'react-navigation';
+import UserElement from './component/UserElement';
+import Storage from '../Storage';
+import { apiKey } from '../GlobalConfig'
 
 export default class MainScreen extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      inGame: false,
+      summoners: []
+    }
+  }
+
+  componentDidMount = () => {
+    console.log("Main screen mounted")
+  }
+
+  updateSummoners = async () => {
+    this.setState({
+      summoners: await Storage.getUsers()
+    })
+  }
+
+  summonerSelected = (summoner) => {
+    console.log(summoner)
+    axios.get(`https://${summoner.region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${userNameInput}`, { 
+      headers: {
+        "X-Riot-Token": apiKey,
+        withCredentials: true
+      }}
+    ).then((response) => {
+      
+    })
+  }
+
+  renderSummonerSelection = ({item, index}) => {
+    console.log(item)
+    return (<UserElement key={item.id} user={item} onSelect={() => this.summonerSelected(item)}/>)
+  }
+  
+
   render() {
     return (
       <View style={styles.container}>
-        
+        <NavigationEvents onWillFocus={this.updateSummoners}/>
+        <FlatList
+          style={{flex: 1}}
+          data={this.state.summoners}
+          renderItem={this.renderSummonerSelection}
+          keyExtractor={(item) => item.id}
+        />
       </View>
     );
   }
@@ -16,8 +63,8 @@ export default class MainScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    //justifyContent: 'center',
+    //alignItems: 'center',
     backgroundColor: '#311e3e',
   },
 });
